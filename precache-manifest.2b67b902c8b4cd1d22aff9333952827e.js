7d3af8df9533fb7f34351ab71a280b20d739a485
Gobdomino <nateball13@gmail.com>
2019-04-06 14:44:42 -0400

    Updates

---
+++ precache-manifest.2b67b902c8b4cd1d22aff9333952827e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13a9891022f530ae1486",
+    "url": "/my-click-game-new/static/js/runtime~main.5d41b36b.js"
+  },
+  {
+    "revision": "529fd03701d1ebd1bff3",
+    "url": "/my-click-game-new/static/js/main.a9ac0ced.chunk.js"
+  },
+  {
+    "revision": "4743c039c04edea7e174",
+    "url": "/my-click-game-new/static/js/2.1db7aab6.chunk.js"
+  },
+  {
+    "revision": "529fd03701d1ebd1bff3",
+    "url": "/my-click-game-new/static/css/main.a378fc7a.chunk.css"
+  },
+  {
+    "revision": "d2321b776ee056501a2e245c36b98e4b",
+    "url": "/my-click-game-new/index.html"
+  }
+];
